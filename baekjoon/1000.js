69d0ac555ae9535f17ab32bdf31bc6d271b67d91
tangjinlog <lkg70007@gmail.com>
2023-01-04 16:14:49 +0900

    baekjoon: 1001

--- baekjoon/1000.js
+++ baekjoon/1000.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const input = fs.readFileSync('test.txt').toString().split(' ');
 const a = input[0]
 const b = input[1]
-console.log(Number(a)+Number(b))
+console.log(Number(a)-Number(b))
