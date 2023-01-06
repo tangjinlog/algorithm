49c27fa3905b2049abbd904521dc65703c82d72e
tangjinlog <lkg70007@gmail.com>
2023-01-07 01:36:45 +0900

    pgm: lv1

--- programmers/문자열나누기.js
+++ programmers/문자열나누기.js
@@ -25,4 +25,20 @@ solution(s)
 /**
  * for of 문으로 하나씩 돌면서 문자열을 담는데,
  * 초깃값을 고정해주기 위해 a라는 변수에 처음 ''를 할당.
+ * 
+ * 다른 분의 풀이
+ * 
+function solution(s, count=0) {
+    if(!s) return count
+    let [first, ...rest] = s.split("")
+    let countSame = 1
+    let countInSame = 0
+    let i=0
+    for(; i<rest.length; i++){
+        if(rest[i] === first) countSame++
+        else countInSame++
+        if(countSame === countInSame) break
+    }
+    return solution(rest.slice(i+1).join(""), count+1)
+}
  */
